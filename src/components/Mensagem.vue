2878439beb6b268cf130e1b09806e1c401bb3691
Filipi Cruz <116102643+FilipiR22@users.noreply.github.com>
2025-11-06 11:50:58 +0000

    mais testes dsobre vue e componentização

---
+++ src/components/Mensagem.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>Este é o primeiro componente Vue!</p>
+</template>
+
+<script setup></script>
+
+<style scoped>
+p {
+    color: #c9c5c5;
+    font-size: 1.1rem;
+}
+</style>
---
+++ src/components/OlaMundo.vue
@@ -0,0 +1,17 @@
+<!-- src/components/OlaMundo.vue -->
+<template>
+  <h1>{{ mensagem }}</h1>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const mensagem = ref('Olá, Mundo!')
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+  text-align: center;
+}
+</style>
